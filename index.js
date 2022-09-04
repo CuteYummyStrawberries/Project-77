4bda66e2567bbc802b989d653ff854f3b1e365ec
CuteYummyStrawberries <105526350+CuteYummyStrawberries@users.noreply.github.com>
2022-09-04 12:52:09 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var first_name = "Ranbir";
+        var last_name = "Kapoot";
+       
+        first_name.concat(last_name);
+        console.log(last_name);
+
+        var a = 10;
+        var b = 5;
+
+        var add = a + b;
+        console.log(add);
